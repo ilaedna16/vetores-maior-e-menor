1893926bf82cf38c7553b546ed4d95395b883e4a
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-04 08:41:51 -0300

    Add files via upload

---
+++ ilaedna.cpp
@@ -0,0 +1,16 @@
+0#include <iostream>
+ main () {
+ 
+ float vetor  [5]; 
+  int v; 
+  
+ for (v=0; v<5; v++) { 
+ scanf ("%f", vetor[v]);  
+ }
+ 
+ for (v=0; v<5; v++){ 
+ if (valor[v] <= 10){ 
+ 
+printf ("A[%d] = %.1\n", v, vetor[v]); 
+    }
+} 
